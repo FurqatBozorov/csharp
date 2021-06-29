3379a92164228e978e1c1bba4a0a7115f5571af0
Furqat <furqatb@gmail.com>
2021-06-29 18:12:14 +0200

    initial commit

---
+++ MySuperBank/MySuperBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Furqat", 20000);
+            Console.WriteLine($"This {account.Number} account was created for {account.Owner} with {account.Balance} initial balance");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(100, DateTime.Now, "Friend paid me back");
+            account.MakeWithdrawal(250, DateTime.Now, "tax");
+            
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+
+
+        }
+    }
+}
